03ea5c454bac801c223e0493b128905b6253a91a
Jeetjyoti Deka <jeetjyoti2020@gmail.com>
2024-05-11 21:59:33 +0530

    Implement Hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,73 @@
+import { Button } from "@/components/ui/button";
+import { Plus, Star, ThumbsUp } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+    <main className="relative">
+      <div className="absolute inset-0 w-full h-screen z-10 bg-black/10 backdrop-blur-sm md:hidden" />
+      <div className="absolute inset-0 w-full h-screen z-0">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          src="/hero-2.png"
+          width={1440}
+          height={1024}
+          alt="hero"
+          className="w-full h-screen object-cover"
         />
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <div className="relative z-50 flex flex-col h-screen justify-center items-center sm:items-start text-white px-4 sm:px-16">
+        <h1 className="text-2xl text-center mt-24 sm:mt-0 xs:text-4xl md:text-5xl lg:text-6xl font-bold">
+          Try Before Committing
+        </h1>
+        <p className="sm:text-lg text-center sm:text-left md:text-2xl mt-10 sm:ml-1.5 max-w-prose">
+          Out of hundreds of books, <br />
+          <span className="bg-primary p-1 rounded-sm font-semibold">
+            CHOOSE
+          </span>{" "}
+          the right one for you.
+        </p>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        <div className="flex flex-col xs:flex-row flex-wrap sm:flex-nowrap items-center xs:items-start justify-center max-w-3xl mt-10 xs:mt-20 gap-5 sm:gap-0">
+          <div className="flex flex-col items-center sm:items-start justify-center">
+            <h3 className="flex items-center justify-center md:text-2xl font-semibold">
+              10,000
+              <Plus
+                className="h-5 w-5 text-primary font-bold"
+                style={{
+                  strokeWidth: "3.5px",
+                }}
+              />
+            </h3>
+            <p className="text-zinc-900 md:text-lg font-normal mt-1">
+              Titles Available
+            </p>
+          </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+          <div className="h-16 hidden sm:block w-px bg-zinc-400 sm:mx-5 " />
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          <div className="flex flex-col items-center sm:items-start justify-center my-6 xs:my-0">
+            <h3 className="flex items-center justify-center md:text-2xl font-semibold">
+              4.5
+              <Star className="h-5 w-5 ml-1 text-primary fill-primary font-bold" />
+            </h3>
+            <p className="text-zinc-900 md:text-lg font-normal mt-1">
+              Average Rating
+            </p>
+          </div>
+
+          <div className="h-16 hidden sm:block w-px bg-zinc-400  sm:mx-5" />
+
+          <div className="flex flex-col items-center sm:items-start justify-center col-span-2">
+            <h3 className="flex items-center justify-center md:text-2xl font-semibold whitespace-nowrap">
+              Glowing Feedback
+              <ThumbsUp className="h-5 w-5 ml-1.5 text-transparent fill-primary" />
+            </h3>
+            <p className="text-zinc-900 md:text-lg font-normal mt-1">
+              &quot;Top Notch Service&quot;
+            </p>
+          </div>
+        </div>
       </div>
     </main>
   );
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+import { ArrowRight } from "lucide-react";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-white/60 fixed z-50 left-0 top-0 inset-x-0 h-14 flex items-center justify-between px-16 border-b border-gray-200 shadow-lg backdrop-blur-xl transition-all">
+      <Link href="/" className="font-semibold text-lg cursor-pointer">
+        Book<span className="text-primary">Case</span>
+      </Link>
+      <Link href="" className={buttonVariants({})}>
+        Rent Now
+        <ArrowRight className="h-5 w-5 ml-1.5" />
+      </Link>
+    </nav>
+  );
+};
+export default Navbar;
